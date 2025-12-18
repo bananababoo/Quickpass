7cd1662ac0033c816532662d380232c373f8f9f0
Bananababooy <48812049+bananababoo@users.noreply.github.com>
2025-12-18 12:28:11 -0600

    change aws stuff

--- server/utils/dynamodb.ts
+++ server/utils/dynamodb.ts
@@ -9,6 +9,10 @@ export default function useDynamoDB() {
     client = DynamoDBDocumentClient.from(
       new DynamoDBClient({
         region: 'us-east-2',
+        credentials: {
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+        }
       })
     )
   }
